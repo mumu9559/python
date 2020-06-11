33bc920334d867c576058916eb6726082e3ffea4
mumu9559 <66147560+mumu9559@users.noreply.github.com>
2020-06-10 21:16:17 -0400

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.version)
+
+activity = input("Enter your favorite activity: ")
+color = input("Enter your favorite color: ")
+food = input("Enter your favorite food: ")
+
+print("I love to" + activity)
+print("I love the color" + color)
+print("I love to eat" + food)
+
